e977c4e58caa57269f058e114734f3ebe950fedb
CodergirlVS <vaidehis1214@gmail.com>
2022-06-11 23:23:50 -0700

    answers for lesson 1

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -3,6 +3,7 @@ console.log('start')
 const first = readFileSync('./content/first.txt', 'utf8')
 const second = readFileSync('./content/second.txt', 'utf8')
 
+console.log(first, second)
 writeFileSync(
   './content/result-sync.txt',
   `Here is the result : ${first}, ${second}`,
---
+++ 01-node-tutorial/answers/03-modules.js
@@ -0,0 +1,9 @@
+//Modules
+const names = require("./04-names.js");
+const sayHi = require("./05-utils");
+const data = require("./06-alternative-flavor");
+require('./07-mind-grenade')
+
+sayHi("Susan");
+sayHi(names.john);
+sayHi(names.peter);
