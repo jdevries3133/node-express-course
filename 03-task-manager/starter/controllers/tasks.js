0fd20f95c7e167aea18597d24b005df220d1e9d0
CodergirlVS <vaidehis1214@gmail.com>
2022-07-10 19:51:49 -0700

    Created 2nd route with try catch block with mongoose model

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,12 +1,22 @@
 const Task = require('../models/Task')
-console.log(Task, "Task")
-const getAllTasks = (req, res) => {
-    res.send('get all tasks')
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({tasks})
+    } catch(error) {
+        res.status(500).json({msg: error.message})
+    }
+    
 }
 
 const createTask = async (req, res) => {
+    try {
     const task = await Task.create(req.body)
     res.status(201).json({task});
+    }catch(error) {
+        res.status(500).json({msg: error.message})
+    }
 }
 
 const getTask = (req, res) => {
