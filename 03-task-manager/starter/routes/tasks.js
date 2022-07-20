479d0e0bad39238eaca32edce55a68b7d8211d1e
CodergirlVS <vaidehis1214@gmail.com>
2022-07-19 21:36:30 -0700

    All files are updated

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -5,10 +5,11 @@ const {getAllTasks,
     createTask,
     getTask,
     updateTask,
-    deleteTask,} = require('../controllers/tasks')
+    deleteTask,
+    editTask} = require('../controllers/tasks')
 
 router.route('/').get(getAllTasks).post(createTask)
 
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask).put(editTask)
 
 module.exports = router;
