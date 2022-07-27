d7cf95ed11faa275c40ecb533c0ca0577c5b6919
CodergirlVS <vaidehis1214@gmail.com>
2022-07-26 21:22:40 -0700

    Limit-to limit the number of results, skip will skip the results from top

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,31 +1,40 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name -price')
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10).skip(1);
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
   if (company) {
-    queryObject.company = company
+    queryObject.company = company;
   }
-  if(name) {
-    queryObject.name = { $regex: name, $options: 'i' };
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
   }
-  let result = Product.find(queryObject)
+  let result = Product.find(queryObject);
   //sort
   if (sort) {
-    const sortList = sort.split(',').join(' ');
+    const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
-    result = result.sort('createdAt');
+    result = result.sort("createdAt");
   }
-  const products = await result
+  //select fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
