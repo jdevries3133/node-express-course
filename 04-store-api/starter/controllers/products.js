46ba50229c80b3682091ec1e971c57d0c68cc544
CodergirlVS <vaidehis1214@gmail.com>
2022-07-26 21:36:00 -0700

    pagination, limits

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -4,7 +4,6 @@ const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({})
     .sort("name")
     .select("name price")
-    .limit(10).skip(1);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -33,6 +32,14 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  //23
+  // 4 pages - each page has following items 7 7 7 2 
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
